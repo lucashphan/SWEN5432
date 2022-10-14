ccf11e9adfdc4e4daca94216949d1fc00ece1511
lucashphan <99146431+lucashphan@users.noreply.github.com>
2022-10-14 11:26:35 -0500

    v1.04

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,66 +7,87 @@ namespace Project
         static void Main()
         {
             float x, y;
-            char operation;
+            char operation, again;
 
-            Console.WriteLine("Please type in your first integer: ");
-            x = float.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Please type in your first integer: ");
+                x = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Now type in your second integer: ");
-            y = float.Parse(Console.ReadLine());
+                Console.WriteLine("Now type in your second integer: ");
+                y = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Type in the operator: ");
-            operation = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("Type in the operator: +, -, *");
+                operation = Convert.ToChar(Console.ReadLine());
 
-            Console.Clear();
+                Console.Clear();
 
-            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-            Console.WriteLine("Conversions of first integer:");
-            Console.WriteLine("Decimal to Binary: " + DecToBinary(x));           
-            Console.WriteLine("Binary to Decimal: " + BinaryToDec(DecToBinary(x)));
-            Console.WriteLine("Decimal to Hex: " + DecToHex(x));
-            Console.WriteLine("Hex to Decimal: " + HexToDex(DecToHex(x)));
-            Console.WriteLine("Decimal to Sign Exponent Mantissa: " + FloatToSEM(x));
-            Console.WriteLine("Sign Exponent Mantissa to Decimal: " + SEMToFloat(FloatToSEM(x)));
-            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-            Console.WriteLine("");
+                Console.WriteLine("==============================================================================");
+                Console.WriteLine("Conversions of first integer: " + x);
+                Console.WriteLine("Decimal to Binary: " + DecToBinary(x));
+                Console.WriteLine("Binary to Decimal: " + BinaryToDec(DecToBinary(x)));
+                Console.WriteLine("Decimal to Hex: " + DecToHex(x));
+                Console.WriteLine("Hex to Decimal: " + HexToDex(DecToHex(x)));
+                Console.WriteLine("Decimal to Sign Exponent Mantissa: " + FloatToSEM(x));
+                Console.WriteLine("Sign Exponent Mantissa to Decimal: " + SEMToFloat(FloatToSEM(x)));
+                Console.WriteLine("==============================================================================");
+                Console.WriteLine("");
 
-            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-            Console.WriteLine("Conversions of second integer:");
-            Console.WriteLine("Decimal to Binary: " + DecToBinary(y));
-            Console.WriteLine("Binary to Decimal: " + BinaryToDec(DecToBinary(y)));
-            Console.WriteLine("Decimal to Hex: " + DecToHex(y));
-            Console.WriteLine("Hex to Decimal: " + HexToDex(DecToHex(y)));
-            Console.WriteLine("Decimal to Sign Exponent Mantissa: " + FloatToSEM(y));
-            Console.WriteLine("Sign Exponent Mantissa to Decimal: " + SEMToFloat(FloatToSEM(y)));
-            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-            Console.WriteLine("");
+                Console.WriteLine("==============================================================================");
+                Console.WriteLine("Conversions of second integer: " + y);
+                Console.WriteLine("Decimal to Binary: " + DecToBinary(y));
+                Console.WriteLine("Binary to Decimal: " + BinaryToDec(DecToBinary(y)));
+                Console.WriteLine("Decimal to Hex: " + DecToHex(y));
+                Console.WriteLine("Hex to Decimal: " + HexToDex(DecToHex(y)));
+                Console.WriteLine("Decimal to Sign Exponent Mantissa: " + FloatToSEM(y));
+                Console.WriteLine("Sign Exponent Mantissa to Decimal: " + SEMToFloat(FloatToSEM(y)));
+                Console.WriteLine("==============================================================================");
+                Console.WriteLine("");
 
-            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-            Console.WriteLine("Operation result: " + MathOperation(x, y, operation));
-            Console.WriteLine("");
-            Console.WriteLine("Conversions of result:");
-            Console.WriteLine("Decimal to Binary: " + DecToBinary(MathOperation(x, y, operation)));
-            Console.WriteLine("Binary to Decimal: " + BinaryToDec(DecToBinary(MathOperation(x, y, operation))));
-            Console.WriteLine("Decimal to Hex: " + DecToHex(MathOperation(x, y, operation)));
-            Console.WriteLine("Hex to Decimal: " + HexToDex(DecToHex(MathOperation(x, y, operation))));
-            Console.WriteLine("Decimal to Sign Exponent Mantissa: " + FloatToSEM(MathOperation(x, y, operation)));
-            Console.WriteLine("Sign Exponent Mantissa to Decimal: " + SEMToFloat(FloatToSEM(MathOperation(x, y, operation))));
-            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-            Console.WriteLine("");
+                Console.WriteLine("==============================================================================");
+                Console.WriteLine("Operation result: " + MathOperation(x, y, operation));
+                Console.WriteLine("");
+                Console.WriteLine("Conversions of result: " + MathOperation(x, y, operation));
+                Console.WriteLine("Decimal to Binary: " + DecToBinary(MathOperation(x, y, operation)));
+                Console.WriteLine("Binary to Decimal: " + BinaryToDec(DecToBinary(MathOperation(x, y, operation))));
+                Console.WriteLine("Decimal to Hex: " + DecToHex(MathOperation(x, y, operation)));
+                Console.WriteLine("Hex to Decimal: " + HexToDex(DecToHex(MathOperation(x, y, operation))));
+                Console.WriteLine("Decimal to Sign Exponent Mantissa: " + FloatToSEM(MathOperation(x, y, operation)));
+                Console.WriteLine("Sign Exponent Mantissa to Decimal: " + SEMToFloat(FloatToSEM(MathOperation(x, y, operation))));
+                Console.WriteLine("==============================================================================");
+                Console.WriteLine("");
+
+                Console.WriteLine("Would you like to run again? Y/N");
+                again = Convert.ToChar(Console.ReadLine());
+            }
+            while (again == 'y');
         }        
         //Convert to Decimal to Binary
         static string DecToBinary(float z)
         {
-            const int bitCount = sizeof(float) * 8;
-            int bin = System.BitConverter.ToInt32(BitConverter.GetBytes(z), 0);
-            return Convert.ToString(bin, 2).PadLeft(bitCount, '0');
+            string s = z.ToString("R");
+            int myInt;
+            bool isNumerical = int.TryParse(s, out myInt);
+            if (isNumerical)
+            {
+                return Convert.ToString(myInt, 2);
+            }
+            else
+            {
+                return "Cannot convert " + s ;
+            }
         }
         //Convert Binary to Decimal
-        static float BinaryToDec(string z)
+        static string BinaryToDec(string z)
         {
-            int sem = Convert.ToInt32(z, 2);
-            return BitConverter.ToSingle(BitConverter.GetBytes(sem), 0);
+            if (z.Contains(".")){
+                return z;
+            }
+            else
+            {
+                int myInt = Convert.ToInt32(z, 2);
+                return myInt.ToString();
+            }
         }
         //Convert to Decimal to Hexadecimal
         static string DecToHex(float z)
